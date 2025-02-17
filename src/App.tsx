942c9d108cfa4d1aafd39bd3195f3c9bb134b2f8
NateG <nate.gantt@adp.com>
2025-02-17 16:12:43 -0500

    Expanded eslint for 'prod-like apps'

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { AppShell, AppShellMain, Text, Title } from '@mantine/core';
 
 function App() {
   return (
-    <AppShell padding="md">
+    <AppShell padding={'md'}>
       <AppShellMain className="flex justify-center items-center">
         <Title className="text-center">
-          <Text inherit variant="gradient" component="span" gradient={{ from: 'blue', to: 'pink' }}>
+          <Text inherit variant={'gradient'} component={'span'} gradient={{ from: 'blue', to: 'pink' }}>
             Things to come!
           </Text>
         </Title>
