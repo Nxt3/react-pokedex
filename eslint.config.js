00a795bfbe05f0b1cbfe9ebf3ed44270302c9991
NateG <nate.gantt@adp.com>
2025-02-17 16:12:43 -0500

    feat(#2): Expanded eslint for 'prod-like apps'

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,35 @@
-import js from '@eslint/js';
+import tsLint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { defineFlatConfig } from 'eslint-define-config';
+import checkFile from 'eslint-plugin-check-file';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineFlatConfig([
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    ignores: ['./dist']
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parser: tsParser,
+      globals: { ...globals.browser }
     },
     plugins: {
+      react,
+      '@typescript-eslint': tsLint,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
+      'check-file': checkFile
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       quotes: [
         'error',
@@ -27,7 +38,22 @@ export default tseslint.config(
           allowTemplateLiterals: true,
           avoidEscape: true
         }
+      ],
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/*.{ts,tsx}': 'KEBAB_CASE'
+        },
+        {
+          ignoreMiddleExtensions: true
+        }
+      ],
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          'src/**/!(__tests__)': 'KEBAB_CASE'
+        }
       ]
     }
   }
-);
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { MantineProvider } from '@mantine/core';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './app.tsx';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
