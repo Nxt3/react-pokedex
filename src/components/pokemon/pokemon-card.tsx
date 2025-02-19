5f7efedd7eb87268ecc82df60f8a13b50a7ce162
NateG <nate.gantt@adp.com>
2025-02-18 22:55:19 -0500

    Use a fixed width for the Pokemon's card to prevent grow/shrink

--- src/components/pokemon/pokemon-card.tsx
+++ src/components/pokemon/pokemon-card.tsx
@@ -4,17 +4,21 @@ import { Pokemon } from '../../types/pokemon';
 
 export function PokemonCard({ pokemon }: { pokemon?: Pokemon }) {
   return (
-    <div className="flex flex-row">
-      <Card shadow="md">
+    <div className="flex">
+      <Card shadow="md" radius="md" padding="lg" withBorder className="w-12 flex-grow-0 flex-shrink-0">
         {pokemon ? (
           <>
             {
               <div className="flex flex-col items-center">
-                <Image src={pokemon.sprites.front_default} alt={pokemon.name} />
+                <Card.Section>
+                  <Image src={pokemon.sprites.front_default} alt={pokemon.name} />
+                </Card.Section>
+
                 <Text ta="center">{pokemon.name.toUpperCase()}</Text>
+
                 <Group gap="5" className="mt-1">
                   {pokemon.types.map(({ type }, index) => (
-                    <Pill key={index}>{type.name}</Pill>
+                    <Pill key={index}>{type.name.toUpperCase()}</Pill>
                   ))}
                 </Group>
               </div>
