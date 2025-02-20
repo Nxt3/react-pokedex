2a63ce73fc708eae8a8abbbe0fe73d5cab84a98e
NateG <nate.gantt@adp.com>
2025-02-20 16:36:51 -0500

    Implemented Light/Dark mode toggle

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -1,13 +1,31 @@
-import { AppShell as MantineAppShell, AppShellMain, Group, Title } from '@mantine/core';
+import {
+  ActionIcon,
+  AppShell as MantineAppShell,
+  AppShellMain,
+  Group,
+  Title,
+  useComputedColorScheme,
+  useMantineColorScheme
+} from '@mantine/core';
 
+import { getColorSchemeToggleProps } from '../utils/theme-manager';
 import PokemonContainer from './pokemon-container';
 
 function AppShell() {
+  const { toggleColorScheme } = useMantineColorScheme();
+  const colorSchemeToggleProps = getColorSchemeToggleProps(useComputedColorScheme());
+
   return (
     <MantineAppShell padding="md" header={{ height: 64 }}>
       <MantineAppShell.Header>
-        <Group h="100%" px="md">
+        <Group h="100%" px="md" justify="space-between">
           <Title>React Pokédex</Title>
+
+          <div>
+            <ActionIcon size="lg" variant="light" color={colorSchemeToggleProps.color} onClick={toggleColorScheme}>
+              {colorSchemeToggleProps.icon}
+            </ActionIcon>
+          </div>
         </Group>
       </MantineAppShell.Header>
 
