c2f914e6351824aac65a1eb5e5e30517422a83f7
NateG <nate.gantt@adp.com>
2025-02-20 15:17:08 -0500

    feat(#5): Added Pokemon.id to card and adjusted card styles

--- src/components/pokemon/pokemon-card.tsx
+++ src/components/pokemon/pokemon-card.tsx
@@ -7,9 +7,9 @@ import { pokemonTypeColorMapper } from '../../utils/pokemon-type-color-mapper';
 
 const baseCardProps: CardProps = {
   shadow: 'md',
-  radius: 'md',
+  radius: 'lg',
   withBorder: true,
-  className: 'w-12 h-12'
+  className: 'w-12 h-14'
 };
 
 export function PokemonCard({ pokemon, isLoading }: { pokemon?: Pokemon; isLoading: boolean }) {
@@ -41,37 +41,46 @@ export function PokemonCard({ pokemon, isLoading }: { pokemon?: Pokemon; isLoadi
           {pokemon ? (
             <Fragment>
               {
-                <Stack align="center" justify="center" gap="xs">
-                  <Card.Section>
-                    <Image src={pokemon.sprites.front_default} alt={pokemon.name} fallbackSrc={pokeball} className="mt-1" />
-                  </Card.Section>
+                <Fragment>
+                  <Stack align="flex-end" justify="flex-start" gap="xs">
+                    <Pill size="sm" radius={baseCardProps.radius}>
+                      {pokemon.id}
+                    </Pill>
+                  </Stack>
 
-                  <Text ta="center" fw={800}>
-                    {pokemon.name.toUpperCase()}
-                  </Text>
+                  <Stack align="center" justify="center" gap="xs">
+                    <Card.Section>
+                      <Image src={pokemon.sprites.front_default} alt={pokemon.name} fallbackSrc={pokeball} className="mt-1" />
+                    </Card.Section>
 
-                  <Group gap={8}>
-                    {pokemon.types.map(({ type }, index) => {
-                      const typeName = type.name as PokemonType;
+                    <Text ta="center" fw={800}>
+                      {pokemon.name.toUpperCase()}
+                    </Text>
 
-                      return (
-                        <Pill
-                          key={index}
-                          styles={() => ({
-                            root: {
-                              // NOTE: using outline to circumvent a11y contrast concerns
-                              outline: `2px solid ${pokemonTypeColorMapper(typeName)}`
-                            }
-                          })}
-                        >
-                          <Text inherit fw={600}>
-                            {typeName.toUpperCase()}
-                          </Text>
-                        </Pill>
-                      );
-                    })}
-                  </Group>
-                </Stack>
+                    {/* TODO: refactor this into it's own component */}
+                    <Group gap={10}>
+                      {pokemon.types.map(({ type }, index) => {
+                        const typeName = type.name as PokemonType;
+
+                        return (
+                          <Pill
+                            key={index}
+                            styles={() => ({
+                              root: {
+                                // NOTE: using outline to circumvent a11y contrast concerns
+                                outline: `2px solid ${pokemonTypeColorMapper(typeName)}`
+                              }
+                            })}
+                          >
+                            <Text inherit fw={600}>
+                              {typeName.toUpperCase()}
+                            </Text>
+                          </Pill>
+                        );
+                      })}
+                    </Group>
+                  </Stack>
+                </Fragment>
               }
             </Fragment>
           ) : (
