bd030900055ae4ce85d18cc039f3ae9f39014fd4
NateG <nate.gantt@adp.com>
2025-02-25 16:39:01 -0500

    fix(#9): Changed Vite's outDir and assetsDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'public',
+    assetsDir: 'src/assets'
+  }
 });
